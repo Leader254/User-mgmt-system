9a64235c159363c7ab7367d7dab79b7d5aaa6d3b
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2023-05-12 23:01:09 +0300

    CRUD operations

--- index.js
+++ index.js
@@ -1,111 +1,73 @@
-// const form = document.querySelector('form');
-// const itemList = document.querySelector('#item-list');
-
-// let userList = [];
-
-// form.addEventListener('submit', addItem);
-
-// function addItem(e) {
-//   e.preventDefault();
-
-//   const name = document.querySelector('#name').value;
-//   const id = document.querySelector('#id').value;
-//   const country = document.querySelector('#country').value;
-//   const language = document.querySelector('#languages').value;
-
-//   const user = {
-//     name: name,
-//     id: id,
-//     country: country,
-//     language: language
-//   };
-
-//   userList.push(user);
-
-//   displayItems(userList);
-
-//   form.reset();
-// }
-
-// function displayItems(userList) {
-//   itemList.innerHTML = '';
-
-//   for (let i = 0; i < userList.length; i++) {
-//     const li = document.createElement('li');
-//     li.className = 'user-item';
-
-//     const nameSpan = document.createElement('span');
-//     nameSpan.className = 'name';
-//     nameSpan.appendChild(document.createTextNode(userList[i].name));
-
-//     const idSpan = document.createElement('span');
-//     idSpan.className = 'id';
-//     idSpan.appendChild(document.createTextNode(userList[i].id));
-
-//     const countrySpan = document.createElement('span');
-//     countrySpan.className = 'country';
-//     countrySpan.appendChild(document.createTextNode(userList[i].country));
-
-//     const languageSpan = document.createElement('span');
-//     languageSpan.className = 'language';
-//     languageSpan.appendChild(document.createTextNode(userList[i].language));
-
-//     const updateBtn = document.createElement('button');
-//     updateBtn.className = 'update-btn';
-//     updateBtn.appendChild(document.createTextNode('Update'));
-
-//     const deleteBtn = document.createElement('button');
-//     deleteBtn.className = 'delete-btn';
-//     deleteBtn.appendChild(document.createTextNode('Delete'));
-
-//     li.appendChild(nameSpan);
-//     li.appendChild(idSpan);
-//     li.appendChild(countrySpan);
-//     li.appendChild(languageSpan);
-//     li.appendChild(updateBtn);
-//     li.appendChild(deleteBtn);
-
-//     itemList.appendChild(li);
-
-//     deleteBtn.addEventListener('click', () => {
-//       userList.splice(i, 1);
-//       displayItems(userList);
-//     });
-
-//     updateBtn.addEventListener('click', () => {
-//       document.querySelector('#name').value = userList[i].name;
-//       document.querySelector('#id').value = userList[i].id;
-//       document.querySelector('#country').value = userList[i].country;
-//       document.querySelector('#languages').value = userList[i].language;
-//       userList.splice(i, 1);
-//       displayItems(userList);
-//     });
-//   }
-// }
-
-
 const form = document.querySelector('form');
-      const tableData = document.getElementById('table-data');
+const tableData = document.getElementById('table-data');
+const successMessage = document.getElementById('success-message');
+const viewTableBtn = document.getElementById('view-table-btn');
+const userTable = document.getElementById('user-table');
+const userInfoHeading = document.getElementById('user-info-heading');
 
-      form.addEventListener('submit', (event) => {
-        event.preventDefault();
+let currentRow = null;
 
-        const username = document.getElementById('name').value;
-        const userId = document.getElementById('id').value;
-        const country = document.getElementById('country').value;
-        const language = document.getElementById('languages').value;
-        const password = document.getElementById('pass').value;
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-        const newRow = document.createElement('tr');
-        newRow.innerHTML = `
-          <td>${username}</td>
-          <td>${userId}</td>
-          <td>${country}</td>
-          <td>${language}</td>
-          <td>${password}</td>
-        `;
+  const username = document.getElementById('name').value;
+  const userId = document.getElementById('id').value;
+  const country = document.getElementById('country').value;
+  const language = document.getElementById('languages').value;
+  const password = document.getElementById('pass').value;
+  document.getElementById('pass').type = 'text';
 
-        tableData.appendChild(newRow);
+  if (currentRow) {
+    // update the current row
+    currentRow.cells[0].textContent = username;
+    currentRow.cells[1].textContent = userId;
+    currentRow.cells[2].textContent = country;
+    currentRow.cells[3].textContent = language;
+    currentRow.cells[4].textContent = password;
+    currentRow = null;
+    successMessage.textContent = "Successfully updated your data.";
+  } else {
+    // add a new row
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+      <td>${username}</td>
+      <td>${userId}</td>
+      <td>${country}</td>
+      <td>${language}</td>
+      <td>${password}</td>
+      <td><button class="edit-btn">Edit</button></td>
+      <td><button class="delete-btn">Delete</button></td>
+    `;
+    const editBtn = newRow.querySelector('.edit-btn');
+    const deleteBtn = newRow.querySelector('.delete-btn');
 
-        form.reset();
-      });
+    editBtn.addEventListener('click', () => {
+      // populate the form with the current row's data
+      document.getElementById('name').value = newRow.cells[0].textContent;
+      document.getElementById('id').value = newRow.cells[1].textContent;
+      document.getElementById('country').value = newRow.cells[2].textContent;
+      document.getElementById('languages').value = newRow.cells[3].textContent;
+      document.getElementById('pass').value = newRow.cells[4].textContent;
+      document.getElementById('pass').type = 'text';
+      currentRow = newRow;
+    });
+
+    deleteBtn.addEventListener('click', () => {
+      // delete the current row
+      newRow.remove();
+      successMessage.textContent = "Successfully deleted the row.";
+    });
+
+    tableData.appendChild(newRow);
+    successMessage.textContent = "Successfully added your data.";
+    viewTableBtn.style.display = "block";
+    userInfoHeading.style.display = "block";
+  }
+
+  form.reset();
+});
+
+viewTableBtn.addEventListener('click', () => {
+  userTable.style.display = "table";
+  window.location.href = "#user-table";
+});
